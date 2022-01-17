56c7c165f4b9d0a27796b666f97be0128a5b5250
Taylor Allred <taylor.allred21@gmail.com>
2022-01-17 14:27:09 -0700

    finally the right compare fn get row and col

--- sudoku_solver.ml
+++ sudoku_solver.ml
@@ -1,9 +1,8 @@
 (* Sudoku Solver by Taylor Allred *)
-
 module Coord = struct
   type t = int * int
 
-  let compare (x1, y1) (x2, y2) = x2 - x1 + (y2 - y1)
+  let compare = compare
 end
 
 module Board = Map.Make (Coord)
@@ -13,7 +12,7 @@ type sudoku = { board : int Board.t; dims : int * int }
 let rec range_ab a b = if a >= b then [] else a :: range_ab (a + 1) b
 let range = range_ab 0
 
-let make_board w h =
+let make_empty_board w h =
   let dims = (w, h) in
   let board = Board.empty in
   { board; dims }
@@ -26,7 +25,7 @@ let make_filled_board w h =
          (fun board i ->
            range (w * h)
            |> List.fold_left
-                (fun board j -> Board.add (i, j) ((i + j) mod 10) board)
+                (fun board j -> Board.add (i, j) (((i + j) mod 10) + 1) board)
                 board)
          Board.empty
   in
@@ -43,3 +42,13 @@ let print_board { board; dims = w, h } =
                 | Some elt -> printf (if j mod w = 0 then " %d" else "%d") elt
                 | None -> printf (if j mod w = 0 then " %s" else "%s") "#"));
   print_endline "\n"
+
+let get_row row_i { board; dims = w, h } =
+  List.map (fun col -> Board.find_opt (row_i, col) board) (range (w * h))
+  |> List.filter Option.is_some
+  |> List.map (fun elt -> Option.value elt ~default:0)
+
+let get_col col_i { board; dims = w, h } =
+  List.map (fun row -> Board.find_opt (row, col_i) board) (range (w * h))
+  |> List.filter Option.is_some
+  |> List.map (fun elt -> Option.value elt ~default:0)
